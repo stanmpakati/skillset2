689acb5b0fcc19a66c81753dd1e422c9dadb1192
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-03-21 14:14:54 +0200

    adding freelancer details page

---
+++ lib/ui/screens/freelancer/profile/details.dart
@@ -0,0 +1,241 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:skillset/ui/screens/freelancer/profile/description.dart';
+import 'package:skillset/ui/shared/widgets/rounded_button.dart';
+
+class PersonalDetails extends StatefulWidget {
+  static final String id = "PersonalDetails";
+  @override
+  _PersonalDetailsState createState() => _PersonalDetailsState();
+}
+
+class _PersonalDetailsState extends State<PersonalDetails> {
+  File _image;
+  bool showSpinner = false;
+  @override
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: SafeArea(
+              child: Center(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                'Personal Details',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'BebasNeue',
+                                  fontSize: 50,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(padding: EdgeInsets.symmetric(horizontal:16),
+                            child: Text("Build your profile.",
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.headline6,
+                                ),
+                              ),
+                            ),
+                          SizedBox(
+                            height: 32,
+                          ),
+                          Center(
+                            child: GestureDetector(
+                              onTap: () {
+                                _showPicker(context);
+                              },
+                              child: CircleAvatar(
+                                radius: 55,
+                                //backgroundColor: Color(0xffFDCF09),
+                                child: _image != null
+                                    ? ClipRRect(
+                                        borderRadius: BorderRadius.circular(50),
+                                        child: Image.file(
+                                          _image,
+                                          width: 130,
+                                          height: 130,
+                                          fit: BoxFit.fitHeight,
+                                        ),
+                                      )
+                                    : Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey[200],
+                                            borderRadius:
+                                                BorderRadius.circular(50)),
+                                        width: 100,
+                                        height: 100,
+                                        child: Icon(
+                                          Icons.person_add,
+                                          size: 60,
+                                          color: Colors.grey[800],
+                                        ),
+                                      ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Container(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(),
+                                    ),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'Full Name',
+                                      border: InputBorder.none,
+                                      prefixIcon: Icon(Icons.person),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(),
+                                    ),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'Email Address',
+                                      border: InputBorder.none,
+                                      prefixIcon: Icon(Icons.mail),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(),
+                                    ),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'City',
+                                      border: InputBorder.none,
+                                      prefixIcon: Icon(Icons.home),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(),
+                                    ),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'Country',
+                                      border: InputBorder.none,
+                                      prefixIcon: Icon(Icons.location_on_rounded),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                // ignore: missing_required_param
+                RoundedButton(
+                  text:'Next',
+                  path: _next,
+                    ),
+              ])
+                ),
+            ),
+          )
+    );
+  }
+
+  /// Get from Camera
+  _getFromCamera() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.camera,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  /// Get from gallery
+  _getFromGallery() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _getFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _getFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+   _next() async {
+  FocusScope.of(context).unfocus();
+  setState(() {
+    showSpinner = true;
+  });
+Navigator.pushReplacementNamed(context, FreelancerDescription.id);
+}
+}
