5602d0417e7389c517f7c7eff8dffde2317f5957
stan000 <stanmpakatig@gmail.com>
2021-04-23 18:00:47 +0200

    added dark mode

--- lib/ui/shared/utils/theme.dart
+++ lib/ui/shared/utils/theme.dart
@@ -19,6 +19,30 @@ TextTheme textTheme = TextTheme(
   ),
 );
 
+TextTheme darkTextTheme = TextTheme(
+  headline1: TextStyle(fontFamily: 'BebasNeue', fontSize: 48.0),
+  headline2: TextStyle(fontFamily: 'BebasNeue', fontSize: 32.0),
+  headline3: TextStyle(fontFamily: 'BebasNeue', fontSize: 28.0),
+  headline4: TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w900,
+    // color: Colors.black87,
+  ),
+  headline5: TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w700,
+    color: Colors.blue,
+  ),
+  headline6: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w700),
+  bodyText1: TextStyle(fontWeight: FontWeight.w700),
+  caption: TextStyle(color: Colors.white54),
+  subtitle1: TextStyle(
+    fontFamily: 'BebasNeue',
+    fontSize: 32.0,
+    // color: Colors.black,
+  ),
+);
+
 // Main theme for light mode
 ThemeData lightTheme = ThemeData(
   // Colors
@@ -33,6 +57,31 @@ ThemeData lightTheme = ThemeData(
   primaryTextTheme: textTheme,
 
   scaffoldBackgroundColor: Color(0xFFf2f2f2),
+
+  // Stypling the app bar
+  appBarTheme: AppBarTheme(
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        fontFamily: 'BebasNeue',
+        fontSize: 32.0,
+        color: Colors.white,
+      ),
+    ),
+  ),
+);
+
+// Main theme for light mode
+ThemeData darkTheme = ThemeData(
+  // Colors
+  brightness: Brightness.dark,
+  primaryColor: Colors.lightBlue,
+  accentColor: Colors.orange,
+
+  // main font
+  fontFamily: 'Raleway',
+
+  // primaryTextTheme: Typography.material2018().white,
+  primaryTextTheme: darkTextTheme,
 
   // Stypling the app bar
   appBarTheme: AppBarTheme(
